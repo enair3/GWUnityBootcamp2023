fc9269bc215ad25a63e897d1c8f40d03cf303096
Xeovis <122372103+Xeovis@users.noreply.github.com>
2023-07-25 13:56:13 -0500

    👆Main UI

---
+++ Assets/Scripts/sceneSwitcher.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSwitcher : MonoBehaviour
+{
+    public void playGame()
+    {
+        // Replace "test" with your scene
+        // Make sure it's in "Scenes in build" File > Build Settings > and add the scene
+        SceneManager.LoadScene("test");
+    }
+    public void howToPlay() {
+        SceneManager.LoadScene("How to Play");
+    }
+    public void quitGame() {
+        Application.Quit();
+    }
+    public void goToMainMenu() {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
