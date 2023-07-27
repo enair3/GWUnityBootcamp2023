3d5b7d2adc75a3358bad82d5badd7bf79cd242a9
enair3 <91293037+enair3@users.noreply.github.com>
2023-07-27 11:55:52 -0500

    play button goes to Battle Scene now

--- Assets/Scripts/sceneSwitcher.cs
+++ Assets/Scripts/sceneSwitcher.cs
@@ -9,7 +9,7 @@ public class sceneSwitcher : MonoBehaviour
     {
         // Replace "test" with your scene
         // Make sure it's in "Scenes in build" File > Build Settings > and add the scene
-        SceneManager.LoadScene("test");
+        SceneManager.LoadScene("Battle Scene");
     }
     public void howToPlay() {
         SceneManager.LoadScene("How to Play");
