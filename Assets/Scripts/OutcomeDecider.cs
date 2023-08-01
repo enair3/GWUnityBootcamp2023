7dc8db71456ebe77d583a2b31ad172ad88b81372
Azura <77252412+shadesgodown-creator@users.noreply.github.com>
2023-08-01 14:18:38 -0500

    comments for Aidan L code mostly complete

--- Assets/Scripts/OutcomeDecider.cs
+++ Assets/Scripts/OutcomeDecider.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class OutcomeDecider : MonoBehaviour
 {
-    public GameObject player1; //set in inspector
-    public GameObject player2; //set in inspector
+    //player objects, set in inspector
+    public GameObject player1;
+    public GameObject player2;
 
-    GamepadControls gamepadControlsP1;
-    GamepadControls gamepadControlsP2;
+    //player gamepad controls scripts
+    GamepadControls gamepadControlsP1; 
+    GamepadControls gamepadControlsP2; 
 
-    Health healthP1;
-    Health healthP2;
+    //player health scripts
+    Health healthP1; 
+    Health healthP2; 
 
     // Start is called before the first frame update
     void Awake()
@@ -78,6 +81,12 @@ public class OutcomeDecider : MonoBehaviour
                 
                 //player 2 reset charge amount
                 gamepadControlsP2.chargeAmount = 1;
+
+                //player 2 dodge delay goes down by 1 if higher than 0
+                if (gamepadControlsP2.dodgeDelay >= 1)
+                {
+                    gamepadControlsP2.dodgeDelay -= 1;
+                }
             }
 
             //player 1 reset charge amount
@@ -113,6 +122,16 @@ public class OutcomeDecider : MonoBehaviour
                 //resets player 2 for next beat
                 gamepadControlsP2.charged = false;
             }
+
+            //if player 2 misses the beat when player 1 dodges
+            else if (gamepadControlsP2.attacked == false && gamepadControlsP2.dodged == false && gamepadControlsP2.charged == false)
+            {
+                //player 2 dodge delay goes down by 1 if higher than 0
+                if (gamepadControlsP2.dodgeDelay >= 1)
+                {
+                    gamepadControlsP2.dodgeDelay -= 1;
+                }
+            }
         }
 
         //if player 1 charges
@@ -137,40 +156,89 @@ public class OutcomeDecider : MonoBehaviour
                 gamepadControlsP2.chargeAmount = 1;
             }
 
-            //
+            //if player 2 dodges when player 1 charges
             else if (gamepadControlsP2.dodged == true)
             {
+                //resets player 2 for next beat
                 gamepadControlsP2.dodged = false;
             }
 
+            //if player 2 charges when player 1 charges
             else if (gamepadControlsP2.charged == true)
             {
+                //resets player 2 for next beat
                 gamepadControlsP2.charged = false;
+            }
+
+            //if player 2 misses the beat when player 1 charges
+            else if (gamepadControlsP2.attacked == false && gamepadControlsP2.dodged == false && gamepadControlsP2.charged == false)
+            {
+                //player 2 dodge delay goes down by 1 if higher than 0
+                if (gamepadControlsP2.dodgeDelay >= 1)
+                {
+                    gamepadControlsP2.dodgeDelay -= 1;
+                }
             }
         }
 
+        //if player 2 attacks when player 1 misses
         else if (gamepadControlsP2.attacked == true)
         {
+            //resets player 2 for next beat
             gamepadControlsP2.attacked = false;
 
+            //if player 1 misses when player 2 attacks
             if (gamepadControlsP1.attacked == false && gamepadControlsP1.dodged == false && gamepadControlsP1.charged == false)
             {
+                //player 1 takes damage
                 healthP1.health -= gamepadControlsP2.chargeAmount;
 
+                //player 1 reset charge amount
                 gamepadControlsP1.chargeAmount = 1;
+
+                //player 1 dodge delay goes down by 1 if higher than 0
+                if (gamepadControlsP1.dodgeDelay >= 1)
+                {
+                    gamepadControlsP1.dodgeDelay -= 1;
+                }
             }
 
+            //player 2 reset charge amount
             gamepadControlsP2.chargeAmount = 1;
         }
 
+        //if player 2 dodges when player 1 misses
         else if (gamepadControlsP2.dodged == true)
         {
+            //resets player 2 for next beat
             gamepadControlsP2.dodged = false;
+
+            //if player 1 misses when player 2 dodges
+            if (gamepadControlsP1.attacked == false && gamepadControlsP1.dodged == false && gamepadControlsP1.charged == false)
+            {
+                //player 1 dodge delay goes down by 1 if higher than 0
+                if (gamepadControlsP1.dodgeDelay >= 1)
+                {
+                    gamepadControlsP1.dodgeDelay -= 1;
+                }
+            }
         }
 
+        //if player 2 charges when player 1 misses
         else if (gamepadControlsP2.charged == true)
         {
+            //resets player 2 for next beat
             gamepadControlsP2.charged = false;
+
+            //if player 1 misses when player 2 charges
+            if (gamepadControlsP1.attacked == false && gamepadControlsP1.dodged == false && gamepadControlsP1.charged == false)
+            {
+                //player 1 dodge delay goes down by 1 if higher than 0
+                if (gamepadControlsP1.dodgeDelay >= 1)
+                {
+                    gamepadControlsP1.dodgeDelay -= 1;
+                }
+            }
         }
     }
 }
